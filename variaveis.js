84a7a4e15dc2e6b779e0eb64155945cd1906cd9b
rodrigorscd1 <rodrigorscd1@gamil.com>
2024-03-28 11:27:54 -0300

    second commit

--- variaveis.js
+++ variaveis.js
@@ -1,9 +1,12 @@
 var pet = 'cachorro';
+pet = "bird";
 var port = "grande";
 
 const idadePet = 5;
 
 let aluno = "Rodrigo";
 const test = 10;
+
+console.log(`a: ${pet}\nidade: ${idadePet} anos`)
 
 console.log(pet)
